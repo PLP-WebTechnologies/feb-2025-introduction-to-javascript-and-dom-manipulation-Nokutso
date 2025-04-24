a5c85116aa43e4a107454d42d03f5a88a6555b74
Nokutso <shxronjack@gmail.com>
2025-04-24 18:45:05 +0200

    Assignment 5

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ button.addEventListener("click", () => {
     let form = document.getElementsByClassName("theForm")[0];
     let paragraph = document.getElementsByTagName("p")[0];
 
-    greet.textContent = "HEY THERE, " + useName.toUpperCase();
-    greet.style.fontSize = "2rem";
+    greet.textContent = "HEY THERE, " + useName.toUpperCase(); //Changes text content dynamically.
+    greet.style.fontSize = "2rem"; //Modifies CSS styles via JavaScript.
     section1.style.backgroundColor = "#A3C3D9";
     paragraph.textContent = "It’s so nice to meet you! I’m truly glad you’re here✨."
 
-    form.remove();
+    form.remove(); //Adds or removes an element when a button is clicked.
 
     let image = document.getElementsByTagName("img")[0];
     image.setAttribute("src", "https://images.pexels.com/photos/7653977/pexels-photo-7653977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
